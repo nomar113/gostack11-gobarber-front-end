590864a2b2e29ceed832bdac27b8b3ada3886fb1
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-05-09 18:54:48 -0300

    API de Contexto

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn/index';
 import SignUp from './pages/SignUp/index';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Ramon' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
