f215ac088622693e12b450a00d3fb6ec1ee64524
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-05-10 11:14:53 -0300

    Estrutura de Toasts

--- src/component/ToastContainer/index.tsx
+++ src/component/ToastContainer/index.tsx
@@ -6,7 +6,26 @@ import { Container, Toast } from './styles';
 const ToastContainer: React.FC = () => {
   return (
     <Container>
-      <Toast>
+      <Toast hasDescription>
+        <FiAlertCircle />
+        <div>
+          <strong>Aconteceu um erro</strong>
+        </div>
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+      <Toast type="success" hasDescription={false}>
+        <FiAlertCircle />
+        <div>
+          <strong>Aconteceu um erro</strong>
+        </div>
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+      <Toast type="error" hasDescription>
+        <FiAlertCircle />
         <div>
           <strong>Aconteceu um erro</strong>
           <p>Não foi possível fazer login na aplicação</p>
