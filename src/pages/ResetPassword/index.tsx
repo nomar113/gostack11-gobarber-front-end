5324d8a6c1b691e5ac63bb039d68716002b8ef5b
Ramon Paiva <rpaiva@vertigo.com.br>
2020-08-25 21:24:32 -0300

    Criando página de redefinição

---
+++ src/pages/ResetPassword/index.tsx
@@ -0,0 +1,85 @@
+import React, { useRef, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import { useToast } from '../../hooks/toast';
+import getValidationErrors from '../../utils/getValidationErrors';
+import logo from '../../assets/logo.svg';
+import { Container, Content, AnimationContainer, Background } from './styles';
+import Input from '../../component/Input';
+import Button from '../../component/Button';
+
+interface ResetPasswordFormData {
+  password: string;
+  password_confirmation: string;
+}
+
+const ResetPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: ResetPasswordFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          password: Yup.string().required('Senha obrigatória'),
+          password_confirmation: Yup.string()
+            .oneOf([Yup.ref('password'), null], 'Confirmação incorreta')
+            .required('Confirmação de senha obrigatória'),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+
+        history.push('/signin');
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro ao resetar senha',
+          description: 'Ocorreu um erro ao resetar sua senha, tente novamente',
+        });
+      }
+    },
+    [addToast, history],
+  );
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logo} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Resetar senha</h1>
+            <Input
+              name="password"
+              icon={FiLock}
+              placeholder="Nova senha"
+              type="password"
+            />
+            <Input
+              name="password_confirmation"
+              icon={FiLock}
+              placeholder="Confirmação da senha"
+              type="password"
+            />
+            <Button type="submit">Alterar senha</Button>
+          </Form>
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+export default ResetPassword;
